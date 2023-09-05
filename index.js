a63db1e74771320d90b9e27167d8fd771d00e87a
lab_sec_b <gmukesh843@gmail.com>
2023-09-05 20:27:40 +0530

    Final Deploys

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/',require('./routes/index'))
 
+app.get('/',(req,res)=>{
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+})
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
